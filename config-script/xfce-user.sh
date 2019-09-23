94bf487a6be2992445e62a37b8022ac97b4bd585
dd-han <han@dd-han.tw>
2019-09-23 21:27:15 +0800

    Update xfce-user.sh

--- config-script/xfce-user.sh
+++ config-script/xfce-user.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-xfconf-query -c xfce4-session --create -p /general/LockCommand --set "light-locker-command -l" --type string
+# this will hang on This Session is locked
+#xfconf-query -c xfce4-session --create -p /general/LockCommand --set "light-locker-command -l" --type string
+# workaround
+xfconf-query -c xfce4-session -p /general/LockCommand -s "dm-tool lock" --create -t string
+
 mkdir -p ~/.config/xfce4/
 cp -r /installer/config-user/xfce4/* ~/.config/xfce4/
 
